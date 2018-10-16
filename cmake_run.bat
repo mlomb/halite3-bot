3b9e2001eb7a92dfda8e7d41b309093925f28c20
Martín Lombardo <mlomb@users.noreply.github.com>
2018-10-16 13:30:40 -0300

    Initial Commit

---
+++ cmake_run.bat
@@ -0,0 +1,3 @@
+if not exist "Build" mkdir Build
+cd Build && cmake . ..
+pause
---
+++ run_game.bat
@@ -0,0 +1,2 @@
+call make.bat
+halite.exe --replay-directory "Build/replays/" -vvv --width 32 --height 32 "Build/Debug/MyBot.exe" "Build/Debug/MyBot.exe"
